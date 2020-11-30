0d669b1632b3d08cce2fa41ed5a7234c406492ab
Achal Lalwani <achal@Soumyas-MacBook-Pro.local>
2020-11-30 05:36:54 +0530

    Added basic navigation and burger builder components. Add/remove ingredients, proceed to checkout, basic styling of components added.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, {Component}  from 'react';
-import Button from '@material-ui/core/Button';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+
 
 
 class App extends Component {
     render() {
         return (
             <>
-                <Layout />
-                <Button variant="contained" color="secondary">
-                    Hello World
-                </Button>
-                <BurgerBuilder />
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
             </>
         );
     }
 }
 
 export default App;
+
---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import classes from './Backdrop.module.css'
+function backdrop(props) {
+    return props.show ? <div className={classes.Backdrop} onClick={props.clicked} /> : null
+}
+
+export default backdrop;
